03b94bad893e3871c1b6fd0dfda9cb2b56bcff69
NgKaiZhe <kaizhe1991@hotmail.com>
2020-03-18 18:22:07 +0800

    add flameLightedUpDetector effect and GameInput handler

---
+++ Assets/FlameLightedUpDetector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameLightedUpDetector : MonoBehaviour
+{
+    public GameObject redFlame;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            // Debug.Log("Player stay in the box collider!");
+            if (GameInput.GetKeyDown(KeyCode.E))
+            {
+                // Debug.Log("Player stay in and pressed E");
+                redFlame.GetComponent<Animator>().SetBool("FlameStart", true);
+            }
+        }
+    }
+}
---
+++ Assets/GameInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    
+    public static bool GetKeyDown(KeyCode code)
+    {
+        if(false) // Game wasnt start yet
+        {
+            return false;
+        }
+        else if(Input.GetKeyDown(code))
+        {
+            Debug.Log("GameInput get key down called!");
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
