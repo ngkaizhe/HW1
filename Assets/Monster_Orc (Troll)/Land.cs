0980234f73d2bb5764bc0cc3d020f6e50cb33d6c
chuchi0212 <aq22636648@gmail.com>
2020-03-21 14:22:11 +0800

    add some voice

---
+++ Assets/Monster_Orc (Troll)/Land.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Land : MonoBehaviour
+{
+
+    AudioSource land;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        land = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void play1()
+    {
+        land.Play();
+    }
+}
---
+++ Assets/Monster_Orc (Troll)/STEP.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class STEP : MonoBehaviour
+{
+
+    AudioSource step;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        step = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void play()
+    {
+        step.Play();
+    }
+
+    void stop()
+    {
+        step.Stop();
+    }
+}
