ec39f8a3f1653531ee6dd1fa52b6d8046eab16c3
chuchi0212 <aq22636648@gmail.com>
2020-03-21 19:36:41 +0800

    all done

--- Assets/Monster_Orc (Troll)/STEP.cs
+++ Assets/Monster_Orc (Troll)/STEP.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class STEP : MonoBehaviour
 {
@@ -29,5 +30,9 @@ public class STEP : MonoBehaviour
     void stop()
     {
         step.Stop();
+    }
+
+    void ChangeToGameOverScene(){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
