cf31fcaf1322d03850078310b444f2f380deefbb
chuchi0212 <aq22636648@gmail.com>
2020-03-21 13:21:40 +0800

    add animation and controller

---
+++ Assets/Monster_Orc (Troll)/control.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour
+{
+     Animator ani;
+    public GameObject cha;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+         ani = cha.GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+
+    void attackfunction()
+    {
+        ani.SetTrigger("attack");
+    }
+}
