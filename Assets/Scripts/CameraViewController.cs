383f7158158eabbdd2c1edc35ab6eabf8f55b384
ngkaizhe <kaizhe1991@hotmail.com>
2020-05-26 12:34:10 +0800

    camera view change done

---
+++ Assets/Scripts/CameraViewController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraViewController : MonoBehaviour
+{
+    public Transform firstViewPos;
+    public Transform thirdViewPos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            transform.parent = firstViewPos;
+            transform.localPosition = Vector3.zero;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            transform.parent = thirdViewPos;
+            transform.localPosition = Vector3.zero;
+        }
+    }
+}
